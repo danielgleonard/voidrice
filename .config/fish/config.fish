a416ba80982768bbf42f3c3c5aadd0d2cd90a4b4
Dan Leonard <daniel@danleonard.us>
2022-02-16 22:47:41 -0600

    XINITRC

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,7 +5,7 @@ end
 # Start X at login
 if status is-login
     if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
+        exec startx "$XINITRC"
     end
 
     # Switch escape and caps if tty and no passwd required:
