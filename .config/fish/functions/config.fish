596fc860300c73b77f902180a8785b94138e07d9
Dan Leonard <daniel@danleonard.us>
2022-02-16 21:39:58 -0600

    LOCATION environment variable

--- .config/fish/functions/config.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function config --description 'Command the git directory of config files in $HOME'
-	/usr/bin/git --git-dir=$HOME/.dotfiles-config/ --work-tree=$HOME $argv;
-end
